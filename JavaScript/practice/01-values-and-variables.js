a2315a7bec1a3cfc6f101d9c402b5959887bff3c
david <mddavid11204@gmail.com>
2022-09-29 22:37:39 +0530

    Hero Section Design

---
+++ JavaScript/practice/01-values-and-variables.js
@@ -0,0 +1,20 @@
+console.log(" My name is David");
+console.log(56);
+console.log("56");
+let a = 5,
+  b = 6,
+  c = 5;
+let value = add(a, b);
+console.log(value);
+function add(a, b) {
+  let c = a + b;
+  return c;
+}
+david = typeof true;
+console.log(david);
+david = typeof a;
+console.log(david);
+console.log(typeof "23456677");
+console.log(typeof false);
+let d;
+console.log(typeof d);
